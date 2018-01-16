e8ad90b3d23b9d2459800e7c4f6eda7f427b5c0b
章鱼辉辉 <842403078@qq.com>
2018-01-16 18:08:01 +0800

    css打包完成

--- src/app.vue
+++ src/app.vue
@@ -7,4 +7,6 @@
 </style>
 <style>
   @import "./css/common.css";
+  /* @import "./css/common2.css"; */
+  /* 既然common2是公用的css，那么在这里就应该不需要单独引入进来，因此在webpack中就不需要打包两次 */
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from "vue";
 import App from "./app";
-import './css/common2'
-
+import './css/common2' //想要webpack中的root。css起作用，这里需要引入才行
 new Vue({
     el:'#app', 
     render:h=> h(App)
